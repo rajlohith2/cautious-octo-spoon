1621421ff47896f6c1dc3f4507f1be61da2cc505
Lohith Raj S N <37451524+rajlohith2@users.noreply.github.com>
2022-03-19 13:16:40 +0530

    pilot

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose'
-
+import constants from '../constants.js';
 const connectDB = async () => {
     try {
         //database Name
         const databaseName='demomern';
-        const con = await mongoose.connect(`mongodb+srv://admin:ronaldo7@cluster0.dbor1.mongodb.net/AppData?retryWrites=true&w=majority`, { 
+        const con = await mongoose.connect(constants.DATABASE_STRING, { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    });
+    })
         console.log(`Database connected : ${con.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
---
+++ backend/constants.js
@@ -0,0 +1,4 @@
+const constants = {
+    DATABASE_STRING : 'mongodb+srv://admin:wubbalubba@cluster0.wuvd6.mongodb.net/players?retryWrites=true&w=majority'
+}
+export default constants
